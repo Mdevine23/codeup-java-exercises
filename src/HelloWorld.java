d6ec94116ad5f48679aa736acf7b16ad96368460
Mark <mark.devine08@gmail.com>
2023-12-06 10:56:25 -0600

    data types exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,39 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, ");
-        System.out.print("World");
+//        System.out.print("Hello, ");
+//        System.out.print("World");
+        int myFavoriteNumber = 23;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        myNumber += 9;
+//        equivalent to
+//        myNumber = myNumber + 9;
+
+//        int x = 4;
+//        x += 4;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+//        byte num = Byte.MAX_VALUE;
+//        num++;
+//        System.out.println(num);
+
     }
 }
